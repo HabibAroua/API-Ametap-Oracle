d61b4976893ee4771b01b738b520cae930a9280e
Habib Aroua <habib.aroua@sesame.com.tn>
2020-09-12 01:21:11 +0100

    Update AfficheNotif.php

--- Ametap/DataOperation/AfficheNotif.php
+++ Ametap/DataOperation/AfficheNotif.php
@@ -1,5 +1,5 @@
 <?php
-    require_once('C:\oraclexe\trabajo\Ametap\Model\Adherent.php');
+	require_once('C:\oraclexe\trabajo\Ametap\Model\Adherent.php');
 	require_once('C:\oraclexe\trabajo\Ametap\Model\Participation.php');
 	$a=new Adherent();
 	$p=new Participation(1,1,1,1,1,1);
