b717f2da410a0a29818548360a90057c1f8fc9f9
Habib Aroua <habib.aroua@sesame.com.tn>
2020-09-12 01:23:53 +0100

    Update AfficherMembreFamille.php

--- Ametap/DataOperation/AfficherMembreFamille.php
+++ Ametap/DataOperation/AfficherMembreFamille.php
@@ -1,5 +1,5 @@
 <?php
-    require_once('C:\oraclexe\trabajo\Ametap\Model\Adherent.php');
+	require_once('C:\oraclexe\trabajo\Ametap\Model\Adherent.php');
 	$a=new Adherent();
 	echo $a->afficheFamile(3);
 ?>
