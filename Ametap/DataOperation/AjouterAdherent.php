24ba84a09bb7fedb16efaf73e4c49560e9b838ab
Habib Aroua <habib.aroua@sesame.com.tn>
2020-09-12 01:25:15 +0100

    Update AjouterAdherent.php

--- Ametap/DataOperation/AjouterAdherent.php
+++ Ametap/DataOperation/AjouterAdherent.php
@@ -1,19 +1,19 @@
 <?php
-    require_once('C:\oraclexe\trabajo\Ametap\Model\Adherent.php');
-    require_once('C:\oraclexe\trabajo\Ametap\Model\Participant.php');
+	require_once('C:\oraclexe\trabajo\Ametap\Model\Adherent.php');
+    	require_once('C:\oraclexe\trabajo\Ametap\Model\Participant.php');
     
-    $matricule=$_POST['matricule'];
+    	$matricule=$_POST['matricule'];
 	$login=$_POST['login'];
 	$password=$_POST['password'];
 	$date_inscription=date('d').'/'.date('m').'/'.date('Y'); 
-    $p=new Participant();
+    	$p=new Participant();
 	$p->insert($matricule,$date_inscription);
 	$a=new Adherent();
 	$test=$a->insert($matricule,$matricule,$login,$password);
 	
 	if($test==true)
 	{
-		
+		//is it true	
 	}
 	else
 	{
