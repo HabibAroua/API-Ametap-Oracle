184aa95ca7cff5b4d21b4a1cfb57e61b87eb7de1
Habib Aroua <habib.aroua@sesame.com.tn>
2020-09-12 01:22:11 +0100

    Update AfficherConjoint.php

--- Ametap/DataOperation/AfficherConjoint.php
+++ Ametap/DataOperation/AfficherConjoint.php
@@ -1,7 +1,7 @@
 <?php
-    require_once('C:\oraclexe\trabajo\Ametap\Model\Conjoint.php');
+	require_once('C:\oraclexe\trabajo\Ametap\Model\Conjoint.php');
 	require_once('C:\oraclexe\trabajo\Ametap\Model\Adherent.php');
-	
+
 	$c=new Conjoint(0,0,0,0,0,0);
 	$a=new Adherent();
 	echo $c->affiche($a->findMatriculeByLogin($_GET['login']));
