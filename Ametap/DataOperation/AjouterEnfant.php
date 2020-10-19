e4ada413ac3cc74044276b44630f1fb48049e822
Habib Aroua <habib.aroua@sesame.com.tn>
2020-10-19 21:14:36 +0100

    Update AjouterEnfant.php

--- Ametap/DataOperation/AjouterEnfant.php
+++ Ametap/DataOperation/AjouterEnfant.php
@@ -2,38 +2,43 @@
 	require_once('C:\oraclexe\trabajo\Ametap\Model\Enfant.php');
 	require_once('C:\oraclexe\trabajo\Ametap\Model\Adherent.php');
 	require_once('C:\oraclexe\trabajo\Ametap\Model\Participant.php');
-	$id=$_POST['id'];
-	$nom=$_POST['nom'];
-	$prenom=$_POST['prenom'];
-	$date_naissance=$_POST['date_naissance'];
-	$ecole=$_POST['ecole'];
-	
-	$a=new Adherent();
-	$matricule=$a->findMatriculeByLogin($_POST['login']);
-	$e=new Enfant($id,$nom,$prenom,$date_naissance,$ecole,$matricule);
-	$p=new Participant();
-	$date_inscription=date('d').'/'.date('m').'/'.date('Y');
-	$x=$a->nombreEnfant($a->findMatriculeByLogin($_POST['login']));
-	$y=$a->nombreEnfantEnregistrer($a->findMatriculeByLogin($_POST['login']));
-	
-	if($x>$y)
+	if((isset($_POST['id'])) && (isset($_POST['nom'])) && (isset($_POST['prenom'])) && (isset($_POST['date_naissance'])) && (isset($_POST['ecole'])))
 	{
-		$p->insert($e->getId(),$date_inscription);
-		$e->insert($e->getId(),$e->getNom(),$e->getPrenom(),$e->getDate_naissance(),$e->getEcole(),$e->getMatricule());
-	}
-	else
-	{
-		if($x==$y)
+		$id=$_POST['id'];
+		$nom=$_POST['nom'];
+		$prenom=$_POST['prenom'];
+		$date_naissance=$_POST['date_naissance'];
+		$ecole=$_POST['ecole'];
+		$a=new Adherent();
+		$matricule=$a->findMatriculeByLogin($_POST['login']);
+		$e=new Enfant($id,$nom,$prenom,$date_naissance,$ecole,$matricule);
+		$p=new Participant();
+		$date_inscription=date('d').'/'.date('m').'/'.date('Y');
+		$x=$a->nombreEnfant($a->findMatriculeByLogin($_POST['login']));
+		$y=$a->nombreEnfantEnregistrer($a->findMatriculeByLogin($_POST['login']));
+
+		if($x>$y)
 		{
-			echo "Vous ne pouvez pas ajouter enfant";
+			$p->insert($e->getId(),$date_inscription);
+			$e->insert($e->getId(),$e->getNom(),$e->getPrenom(),$e->getDate_naissance(),$e->getEcole(),$e->getMatricule());
 		}
 		else
 		{
-			if($x<$y)
+			if($x==$y)
 			{
-				echo "Vous ne pouvez pas ajouter un nouvel enfant";
+				echo "Vous ne pouvez pas ajouter enfant";
+			}
+			else
+			{
+				if($x<$y)
+				{
+					echo "Vous ne pouvez pas ajouter un nouvel enfant";
+				}
 			}
 		}
 	}
-
+	else
+	{
+		echo "Error !!";
+	}
 ?>
