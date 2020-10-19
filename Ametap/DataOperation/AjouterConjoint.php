5089d5143873ac9f8e091a7931e6abdd3c647203
Habib Aroua <habib.aroua@sesame.com.tn>
2020-10-19 21:12:20 +0100

    Update AjouterConjoint.php

--- Ametap/DataOperation/AjouterConjoint.php
+++ Ametap/DataOperation/AjouterConjoint.php
@@ -4,29 +4,36 @@
 	require_once('C:\oraclexe\trabajo\Ametap\Model\Participant.php');
 	$a=new Adherent();
 	$p=new Participant();
-	$cin=$_POST['cin'];
-	$nom=$_POST['nom'];
-	$prenom=$_POST['prenom'];
-	$date_naissance=$_POST['date_naissance'];
-	$metier=$_POST['metier'];
-	$matricule=$a->findMatriculeByLogin($_POST['login']);
-	$c=new Conjoint($cin,$nom,$prenom,$date_naissance,$metier,$matricule);
-	$date_inscription=date('d').'/'.date('m').'/'.date('Y');
-	if (($a->estMarie($matricule)=='M') && ($a->nombreConjointEnregistrer($matricule)==0))
+	if ((isset($_POST['cin'])) && (isset($_POST['nom'])) && (isset($_POST['prenom'])) && (isset($_POST['date_naissance'])))
 	{
-		$p->insert($c->getCin(),$date_inscription);
-		$c->insert($c->getCin(),$c->getNom(),$c->getPrenom(),$c->getDate_naissance(),$c->getMetier(),$matricule);
-		echo "L'insertion de votre conjoint est effectué avec succes";
-	}
-	else
-	{
-		if($a->nombreConjointEnregistrer($matricule)==1)
+		$cin=$_POST['cin'];
+		$nom=$_POST['nom'];
+		$prenom=$_POST['prenom'];
+		$date_naissance=$_POST['date_naissance'];
+		$metier=$_POST['metier'];
+		$matricule=$a->findMatriculeByLogin($_POST['login']);
+		$c=new Conjoint($cin,$nom,$prenom,$date_naissance,$metier,$matricule);
+		$date_inscription=date('d').'/'.date('m').'/'.date('Y');
+		if (($a->estMarie($matricule)=='M') && ($a->nombreConjointEnregistrer($matricule)==0))
 		{
-			echo 'vous avez un conjoint';
+			$p->insert($c->getCin(),$date_inscription);
+			$c->insert($c->getCin(),$c->getNom(),$c->getPrenom(),$c->getDate_naissance(),$c->getMetier(),$matricule);
+			echo "L'insertion de votre conjoint est effectué avec succes";
 		}
 		else
 		{
-			echo 'Vous êtes célibatire';
+			if($a->nombreConjointEnregistrer($matricule)==1)
+			{
+				echo 'vous avez un conjoint';
+			}
+			else
+			{
+				echo 'Vous êtes célibatire';
+			}
 		}
+	}
+	else
+	{
+		echo "Error !!";
 	}
 ?>
