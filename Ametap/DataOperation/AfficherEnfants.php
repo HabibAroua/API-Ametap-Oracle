24d95f8e026e8747f8f273fc5f557924049a4e0d
Habib Aroua <habib.aroua@sesame.com.tn>
2020-10-19 20:14:06 +0100

    Update AfficherEnfants.php

--- Ametap/DataOperation/AfficherEnfants.php
+++ Ametap/DataOperation/AfficherEnfants.php
@@ -1,8 +1,14 @@
 <?php
 	require_once('C:\oraclexe\trabajo\Ametap\Model\Enfant.php');
 	require_once('C:\oraclexe\trabajo\Ametap\Model\Adherent.php');
-
 	$e=new Enfant(0,0,0,0,0,0);
 	$a=new Adherent();
-	echo $e->afficheEnfant($a->findMatriculeByLogin($_GET['login']));
+	if($_GET['login'])
+	{
+		echo $e->afficheEnfant($a->findMatriculeByLogin($_GET['login']));
+	}
+	else
+	{
+		echo "Error";
+	}
 ?>
