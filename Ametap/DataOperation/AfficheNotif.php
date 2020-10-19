956b15631e4757ac280610273eb7c47da10c8f61
Habib Aroua <habib.aroua@sesame.com.tn>
2020-10-19 20:11:19 +0100

    Update AfficheNotif.php

--- Ametap/DataOperation/AfficheNotif.php
+++ Ametap/DataOperation/AfficheNotif.php
@@ -3,5 +3,8 @@
 	require_once('C:\oraclexe\trabajo\Ametap\Model\Participation.php');
 	$a=new Adherent();
 	$p=new Participation(1,1,1,1,1,1);
-	echo $p->afficheNotf($a->findMatriculeByLogin($_GET['login']));
+	if (isset($_GET['login']))
+	{
+		echo $p->afficheNotf($a->findMatriculeByLogin($_GET['login']));
+	}
 ?>
