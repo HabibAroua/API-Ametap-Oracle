f79addd7e85c9b250a5e814306396a67eabf4e62
Habib Aroua <habib.aroua@sesame.com.tn>
2020-10-19 21:09:25 +0100

    Update AjouterAdherent.php

--- Ametap/DataOperation/AjouterAdherent.php
+++ Ametap/DataOperation/AjouterAdherent.php
@@ -1,24 +1,28 @@
 <?php
 	require_once('C:\oraclexe\trabajo\Ametap\Model\Adherent.php');
     	require_once('C:\oraclexe\trabajo\Ametap\Model\Participant.php');
-    
-    	$matricule=$_POST['matricule'];
-	$login=$_POST['login'];
-	$password=$_POST['password'];
-	$date_inscription=date('d').'/'.date('m').'/'.date('Y'); 
-    	$p=new Participant();
-	$p->insert($matricule,$date_inscription);
-	$a=new Adherent();
-	$test=$a->insert($matricule,$matricule,$login,$password);
-	
-	if($test==true)
+	if((isset($_POST['matricule'])) && (isset($_POST['login'])) && (isset($_POST['password'])))
 	{
-		//is it true	
+		$matricule=$_POST['matricule'];
+		$login=$_POST['login'];
+		$password=$_POST['password'];
+		$date_inscription=date('d').'/'.date('m').'/'.date('Y'); 
+		$p=new Participant();
+		$p->insert($matricule,$date_inscription);
+		$a=new Adherent();
+		$test=$a->insert($matricule,$matricule,$login,$password);
+
+		if($test==true)
+		{
+			//is it true	
+		}
+		else
+		{
+			$p->deletes($matricule);
+		}
 	}
 	else
 	{
-		$p->deletes($matricule);
-	}
-	
-	
+		echo "Error !!";
+	}	
 ?>
