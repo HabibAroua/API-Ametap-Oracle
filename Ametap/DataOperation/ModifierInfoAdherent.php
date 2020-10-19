16a7ee455ea1f3e1b781d7778a6740b031a6b4cc
Habib Aroua <habib.aroua@sesame.com.tn>
2020-10-19 21:32:54 +0100

    Update ModifierInfoAdherent.php

--- Ametap/DataOperation/ModifierInfoAdherent.php
+++ Ametap/DataOperation/ModifierInfoAdherent.php
@@ -1,8 +1,15 @@
 <?php
 	require_once('C:\oraclexe\trabajo\Ametap\Model\Adherent.php');
 	$a=new Adherent();
-	$oldLogin=$_POST['oldLogin'];
-	$newLogin=$_POST['newLogin'];
-	$newPassword=$_POST['newPassword'];
-	$a->modifierInformationPersonnel($oldLogin,$newLogin,$newPassword);
+	if(($_POST['oldLogin']) && ($_POST['newLogin']))
+	{
+		$oldLogin=$_POST['oldLogin'];
+		$newLogin=$_POST['newLogin'];
+		$newPassword=$_POST['newPassword'];
+		$a->modifierInformationPersonnel($oldLogin,$newLogin,$newPassword);
+	}
+	else
+	{
+		echo "Error !";
+	}
 ?>
