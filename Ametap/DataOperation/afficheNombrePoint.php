056149b0756a43f5c90886e4c111684fe0771226
Habib Aroua <habib.aroua@sesame.com.tn>
2020-10-20 22:23:05 +0100

    Update afficheNombrePoint.php

--- Ametap/DataOperation/afficheNombrePoint.php
+++ Ametap/DataOperation/afficheNombrePoint.php
@@ -1,5 +1,8 @@
 <?php
-    require_once('C:\oraclexe\trabajo\Ametap\Model\Adherent.php');
+	require_once('C:\oraclexe\trabajo\Ametap\Model\Adherent.php');
 	$a=new Adherent();
-	echo 'Vous avez '.$a->getNombrePoint($a->findMatriculeByLogin($_POST['login'])).' points';
+	if(isset($_POST['login']))
+	{
+		echo 'Vous avez '.$a->getNombrePoint($a->findMatriculeByLogin($_POST['login'])).' points';
+	}
 ?>
