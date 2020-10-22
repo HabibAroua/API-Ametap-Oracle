cc2a086f8ef262127beedfe47d66bf99ebc316b2
Habib Aroua <habib.aroua@sesame.com.tn>
2020-10-22 18:07:57 +0100

    Update AfficheHistoriqueActivite.php

--- Ametap/DataOperation/AfficheHistoriqueActivite.php
+++ Ametap/DataOperation/AfficheHistoriqueActivite.php
@@ -3,5 +3,12 @@
     require_once('C:\oraclexe\trabajo\Ametap\Model\Adherent.php');
     $adherent=new Adherent();
     $activite=new Activite(0,0,0,0,0,0,0,0,0,0,0,0);
-    echo $activite->AfficheHistorique($adherent->findMatriculeByLogin($_GET['login']));
+    if(isset($_GET['login']))
+    {
+        echo $activite->AfficheHistorique($adherent->findMatriculeByLogin($_GET['login']));
+    }
+    else
+    {
+        echo "Error !!";
+    }
 ?>
