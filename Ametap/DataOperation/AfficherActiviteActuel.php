14de5e0d19391dfc4f12ea8db4b75fd32a7360f2
Habib Aroua <habib.aroua@sesame.com.tn>
2021-11-24 16:58:59 +0100

    Update AfficherActiviteActuel.php

--- Ametap/DataOperation/AfficherActiviteActuel.php
+++ Ametap/DataOperation/AfficherActiviteActuel.php
@@ -1,5 +1,5 @@
 <?php
-    require_once('C:\oraclexe\trabajo\Ametap\Model\Activite.php');
+    	require_once('C:\oraclexe\trabajo\Ametap\Model\Activite.php');
 	$a=new Activite(0,0,0,0,0,0,0,0,0,0,0,0);
 	echo $a->sellectAllActivity();
 ?>
