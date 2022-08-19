c1d110d8f68e48c634d1857485cf555e1db53c05
Habib Aroua <habib.aroua@sesame.com.tn>
2022-08-19 15:54:44 +0100

    Update Connexion.php

--- Ametap/Model/Connexion.php
+++ Ametap/Model/Connexion.php
@@ -1,21 +1,26 @@
 <?php
-    $tns = "  
-    (DESCRIPTION =
-        (ADDRESS_LIST =
-            (ADDRESS = (PROTOCOL = TCP)(HOST =192.168.43.212)(PORT = 1521))
-        )
-        (CONNECT_DATA =
-            (SERVICE_NAME = XE)
-        )
-    )";
+	$tns = "  
+    		(
+			DESCRIPTION =
+        				(
+						ADDRESS_LIST =
+            					(
+							ADDRESS = (PROTOCOL = TCP)(HOST =192.168.43.212)(PORT = 1521)
+						)
+        				)
+        		(
+				CONNECT_DATA =
+            			(SERVICE_NAME = XE)
+        		)
+    		)";
 	$db_username = "Ametap";
-    $db_password = "informatique";
-    try
-    {
-        $conn = new PDO("oci:dbname=".$tns,$db_username,$db_password);
-    }
+    	$db_password = "informatique";
+    	try
+    	{
+        	$conn = new PDO("oci:dbname=".$tns,$db_username,$db_password);
+    	}
 	catch(PDOException $e)
 	{
-        echo ($e->getMessage());
-    }
+        	echo ($e->getMessage());
+    	}
 ?>
